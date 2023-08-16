65e77547879aacc8c338f06f31f8fdfcf66ae5ac
DuBoris Smith <duboris.smith@hotmail.com>
2023-08-16 18:08:48 -0500

    Added functions to videoGames db and api

--- db/videoGames.js
+++ db/videoGames.js
@@ -28,17 +28,50 @@ async function getVideoGameById(id) {
 
 // POST - /api/video-games - create a new video game
 async function createVideoGame(body) {
-    // LOGIC GOES HERE
+    const {name , description, price, inStock, isPopular, imgUrl } = body;
+    try {
+        const { rows: [videoGame] } = await client.query(`
+        
+            INSERT INTO boardgames(name. description, price, "inStock", "isPopular", "imgUrl")
+            VALUES($1, $2, $3, $4, $5, $6)
+            RETURNING *;
+        `, [name, description, price, inStock, isPopular, imgUrl]);
+        return videoGame;     
+    } catch (error) {
+        throw error;
+    }
 }
 
 // PUT - /api/video-games/:id - update a single video game by id
 async function updateVideoGame(id, fields = {}) {
-    // LOGIC GOES HERE
+    const setString = Objects.keys(fields).map((key, index) => `"${key}"=$${index + 1}`).join(', ');
+    if(setString.length === 0) {
+        return;
+    }
+    try {
+        const {rows: [videoGame] } = await client.query(`
+            UPDATE videogames
+            SET ${setString}
+            WHERE ID id${id}
+            RETURNING *;
+        `, Object.values(fields));
+        return videoGame;
+    } catch (error) {
+        throw error;
+    }
 }
 
 // DELETE - /api/video-games/:id - delete a single video game by id
 async function deleteVideoGame(id) {
-    // LOGIC GOES HERE
+    try {
+        const { rows: [videoGame] } = await client.query(`
+            DELETE FROM videogames
+            WHERE id=$1
+            RETURNING *
+        `, [id]);
+    } catch (error) {
+        throw error;
+    }
 }
 
 module.exports = {
